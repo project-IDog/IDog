bb47cbe394c9e1be7afb5aa8c2515ade4521a818
kimmainsain <kmsid003@gmail.com>
2023-09-06 00:37:58 +0900

    feat : eslint, prettier 생성 + Album 주석추가

---
+++ front/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuote: false,
+  arrowParens: "always",
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 80,
+  trailingComma: "all",
+  pluginSearchDirs: false,
+};
--- front/src/components/CommonLayout.tsx
+++ front/src/components/CommonLayout.tsx
@@ -1,22 +1,18 @@
-import { ScrollView, StyleSheet } from 'react-native';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { ScrollView, StyleSheet } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 const CommonLayout = ({ children }: any) => {
   return (
     <>
-        <SafeAreaProvider>
-            <SafeAreaView edges={['top', 'right', 'bottom', 'left']}>
-                <ScrollView>
-                  {children}
-                </ScrollView>
-            </SafeAreaView>
-        </SafeAreaProvider>
+      <SafeAreaProvider>
+        <SafeAreaView edges={["top", "right", "bottom", "left"]}>
+          <ScrollView>{children}</ScrollView>
+        </SafeAreaView>
+      </SafeAreaProvider>
     </>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  
-});
+const styles = StyleSheet.create({});
 
 export default CommonLayout;
