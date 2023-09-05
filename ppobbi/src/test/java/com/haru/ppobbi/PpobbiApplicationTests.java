34b329fdf945fa195162e4e93db17de941ac403f
sunhee3859 <sunhee3859@naver.com>
2023-09-05 14:21:33 +0900

    init

---
+++ ppobbi/src/test/java/com/haru/ppobbi/PpobbiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haru.ppobbi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpobbiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
