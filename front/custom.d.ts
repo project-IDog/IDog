fba3229d902747130a52dc79f4975a9fd2d0149e
kimmainsain <kmsid003@gmail.com>
2023-09-06 09:32:33 +0900

    Fix : custom.d.ts env추가

--- front/custom.d.ts
+++ front/custom.d.ts
@@ -1,14 +1,21 @@
-declare module '*.svg' {
+declare module "*.svg" {
   const content: any;
   export default content;
 }
 
-declare module '*.jpg' {
+declare module "*.jpg" {
   const content: any;
   export default content;
 }
 
-declare module '*.png' {
+declare module "*.png" {
   const content: any;
   export default content;
+}
+
+declare module "@env" {
+  export const AWS_ACCESS_KEY: string;
+  export const AWS_SECRET_ACCESS_KEY: string;
+  export const AWS_REGION: string;
+  export const AWS_BUCKET: string;
 }
