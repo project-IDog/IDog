866e7c76baf7f6e6c118d3f1026c61cf1100ed14
sunhee3859 <sunhee3859@naver.com>
2023-09-07 21:58:35 +0900

    Init(blockchain) : 블록체인 프로젝트 생성

---
+++ blockchain/IDogWallet/babel.config.js
@@ -0,0 +1,20 @@
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        "@babel/plugin-transform-class-properties",
+        { "loose": true }
+      ],
+      [
+        "@babel/plugin-transform-private-methods",
+        { "loose": true }
+      ],
+      [
+        "@babel/plugin-transform-private-property-in-object",
+        { "loose": true }
+      ]
+    ]
+  };
+};
