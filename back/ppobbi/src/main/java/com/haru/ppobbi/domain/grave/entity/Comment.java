46c3065833972041664c97af68d466053f047dd2
Geon <quso12358@naver.com>
2023-09-11 16:38:57 +0900

    Feat(comment): comment DTO 추가
    - comment DTO 추가

---
+++ back/ppobbi/src/main/java/com/haru/ppobbi/domain/grave/entity/Comment.java
@@ -0,0 +1,29 @@
+package com.haru.ppobbi.domain.grave.entity;
+
+import com.haru.ppobbi.domain.user.entity.User;
+import com.haru.ppobbi.global.entity.BaseEntity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "comments")
+@Getter
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_no")
+    private Integer commentNo;
+    @Column(name = "grave_no")
+    private Integer graveNo;
+    @ManyToOne
+    @JoinColumn(name = "user_no")
+    private User user;
+    @Column(name = "comment_content")
+    private String commentContent;
+}
