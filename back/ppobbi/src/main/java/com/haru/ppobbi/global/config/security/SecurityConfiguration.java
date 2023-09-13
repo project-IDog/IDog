5782a1069f61851575625c135fde7d2367d7c3bb
lkk <rabbit1999k@naver.com>
2023-09-13 21:16:46 +0900

    :bug: Fix(security): security access level 수정
    - 테스트 용으로 전부 허용

--- back/ppobbi/src/main/java/com/haru/ppobbi/global/config/security/SecurityConfiguration.java
+++ back/ppobbi/src/main/java/com/haru/ppobbi/global/config/security/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package com.haru.ppobbi.global.config.security;
 
-import com.haru.ppobbi.domain.user.service.OAuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +12,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfiguration {
 
-    private final OAuthService oAuthService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -21,16 +19,12 @@ public class SecurityConfiguration {
             .csrf().disable()
             // 모든 request 허용
             .authorizeRequests()
-            .anyRequest().authenticated()
-
-            // OAuth 적용, 로그인 시
-            .and()
-            .oauth2Login() // OAuth2 로그인 설정 진입점
-            .userInfoEndpoint() // OAuth2 로그인 성공 이후 사용자 정보 가져옴
-            .userService(oAuthService); // 가져온 정보를 토대로 파싱
+            .antMatchers("/**")
+            .permitAll()
+            .anyRequest()
+            .authenticated()
+        ;
 
         return http.build();
     }
-
-
 }
