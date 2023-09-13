e168208e7cafa4e38efaaee9d37667928bd0a72d
Geon <quso12358@naver.com>
2023-09-13 09:50:54 +0900

    Feat(photo/repo): Photo Repository 추가
    - Photo Repository 추가

--- back/ppobbi/src/main/java/com/haru/ppobbi/domain/photo/repo/PhotoRepository.java
+++ back/ppobbi/src/main/java/com/haru/ppobbi/domain/photo/repo/PhotoRepository.java
@@ -1,4 +1,36 @@
 package com.haru.ppobbi.domain.photo.repo;
 
-public interface PhotoRepository {
+import com.haru.ppobbi.domain.photo.entity.Photo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PhotoRepository extends JpaRepository<Photo, Integer> {
+    /**
+     * userNo를 조건으로 하는 모든 Photo 조회
+     * @author Geon
+     * @param userNo User 기본 키
+     * @param canceled 삭제 여부
+     * @return {@code List<Photo>}
+     */
+    public List<Photo> findAllByUserNoAndCanceled(Integer userNo, Integer canceled);
+
+    /**
+     * photoNo를 조건으로 하는 Photo 단일 조회
+     * @author Geon
+     * @param photoNo Photo 기본 키
+     * @param canceled 삭제 여부
+     * @return Photo
+     */
+    public Photo findPhotoByPhotoNoAndCanceled(Integer photoNo, Integer canceled);
+
+    /**
+     * dogNo를 조건으로 하는 모든 Photo 조회
+     * @author Geon
+     * @param dogNo Dog 기본 키
+     * @param canceled 삭제 여부
+     * @return {@code List<Photo>}
+     */
+    public List<Photo> findAllByDogDogNoAAndCanceled(Integer dogNo, Integer canceled);
+
 }
