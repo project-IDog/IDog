3897c7a11ba37f9f1f0b833c31cfdeba96e8ac92
Geon <quso12358@naver.com>
2023-09-13 12:44:29 +0900

    Feat(challenge): Challenge Response Message 추가
    - Challenge Response Message 추가

--- back/ppobbi/src/main/java/com/haru/ppobbi/domain/challenge/constant/ChallengeResponseMessage.java
+++ back/ppobbi/src/main/java/com/haru/ppobbi/domain/challenge/constant/ChallengeResponseMessage.java
@@ -1,4 +1,17 @@
 package com.haru.ppobbi.domain.challenge.constant;
 
 public enum ChallengeResponseMessage {
+    READ_SUCCESS("챌린지 조회 성공"),
+    READ_FAIL("챌린지 조회 실패"),
+    UNAUTHORIZED("인증 실패");
+
+    private final String message;
+
+    ChallengeResponseMessage(String message){
+        this.message = message;
+    }
+
+    public String message(){
+        return this.message;
+    }
 }
