15b490ff672522dc487bae28a3a2a2111c138e0a
lkk <rabbit1999k@naver.com>
2023-09-14 22:57:15 +0900

    :hammer: Refactor(user): OAuth2TokenProvider rename

--- back/ppobbi/src/main/java/com/haru/ppobbi/global/util/oauth/OAuth2TokenProvider.java
+++ back/ppobbi/src/main/java/com/haru/ppobbi/global/util/oauth/OAuth2TokenProvider.java
@@ -13,7 +13,7 @@ import org.springframework.web.client.RestTemplate;
 @Component
 @RequiredArgsConstructor
 @Slf4j
-public class OauthTokenProvider {
+public class OAuth2TokenProvider {
 
     private final RestTemplate restTemplate;
 
