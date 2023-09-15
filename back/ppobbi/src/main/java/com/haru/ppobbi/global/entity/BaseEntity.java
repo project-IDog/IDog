c9440001c9d0a2a2b23c6c1780a1b35dafc60113
lkk <rabbit1999k@naver.com>
2023-09-15 15:25:30 +0900

    :construction_worker: Setting(jpa): jpa base entity auditing listener 추가

--- back/ppobbi/src/main/java/com/haru/ppobbi/global/entity/BaseEntity.java
+++ back/ppobbi/src/main/java/com/haru/ppobbi/global/entity/BaseEntity.java
@@ -1,24 +1,27 @@
 package com.haru.ppobbi.global.entity;
 
-import lombok.Getter;
-
-import javax.persistence.Column;
-import javax.persistence.MappedSuperclass;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @MappedSuperclass
 @Getter
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
+
     @Column(name = "create_date")
     private LocalDateTime createDate;
+
     @Column(name = "modify_date")
     private LocalDateTime modifyDate;
-    @Column(name = "canceled")
-    private Integer canceled;
 
-    public void setCanceled(Integer canceled){
+    @Column(name = "canceled")
+    private int canceled;
+
+    public void setCanceled(Integer canceled) {
         this.canceled = canceled;
     }
 }
