3f9f45d562a1b30800742f27c470749fcce67e11
kimmainsain <kmsid003@gmail.com>
2023-09-17 01:31:08 +0900

    :heavy_plus_sign: Feat : resetButton, SharedPreferencesSetting

--- front/android/app/src/main/java/com/idog/front/StopWatch.java
+++ front/android/app/src/main/java/com/idog/front/StopWatch.java
@@ -3,6 +3,7 @@ package com.idog.front;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.widget.RemoteViews;
 import android.app.PendingIntent;
 import android.content.Intent;
@@ -14,12 +15,34 @@ import android.util.Log;
  */
 public class StopWatch extends AppWidgetProvider {
 
-    // 이 변수들을 추가하세요.
+    private static final String PREFS_NAME = "StopWatchPrefs";
+    private static final String PREF_START_TIME = "startTime";
+    private static final String PREF_ELAPSED_TIME = "elapsedTime";
+
     private static long startTime = 0;
     private static long elapsedTime = 0;
 
+    // SharedPreferences 초기화
+    private static SharedPreferences getPrefs(Context context) {
+        return context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+    }
+
+    private static void loadTime(Context context) {
+        SharedPreferences prefs = getPrefs(context);
+        startTime = prefs.getLong(PREF_START_TIME, 0);
+        elapsedTime = prefs.getLong(PREF_ELAPSED_TIME, 0);
+    }
+
+    private static void saveTime(Context context) {
+        SharedPreferences.Editor editor = getPrefs(context).edit();
+        editor.putLong(PREF_START_TIME, startTime);
+        editor.putLong(PREF_ELAPSED_TIME, elapsedTime);
+        editor.apply();
+    }
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        loadTime(context);
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
@@ -51,14 +74,19 @@ public class StopWatch extends AppWidgetProvider {
             Intent serviceIntent = new Intent(context, TimerService.class);
             serviceIntent.setAction("STOP_ACTION");
             context.startService(serviceIntent);
+            saveTime(context);
+        } else if ("RESET_ACTION".equals(intent.getAction())) {
+            Log.d("WATCH_RESET_ACTION", "WATCH_RESET_ACTION CLICK");
+            elapsedTime = 0;  // elapsedTime 초기화
+            startTime = 0;    // startTime 초기화
+            Intent serviceIntent = new Intent(context, TimerService.class);
+            serviceIntent.setAction("RESET_ACTION");
+            context.startService(serviceIntent);
         } else if (TimerService.ACTION_UPDATE.equals(intent.getAction())) {
             Log.d("WATCH_WHAT_ACTION", "WATCH_WHAT ACTION CLICK");
             elapsedTime += System.currentTimeMillis() - startTime;
             startTime = System.currentTimeMillis();
 
-            // 이제 위젯 UI를 업데이트하십시오.
-            // 시간을 문자열로 변환하는 로직은 단순화되었습니다.
-            // 이를 더 발전시켜 올바른 시간을 표시하도록 해야 합니다.
             String timeStr = String.format("%02d:%02d:%02d",
                     (elapsedTime / 3600000),
                     (elapsedTime / 60000) % 60,
@@ -78,7 +106,6 @@ public class StopWatch extends AppWidgetProvider {
         Intent playIntent = new Intent(context, StopWatch.class);
         playIntent.setAction("PLAY_ACTION");
         PendingIntent playPendingIntent = PendingIntent.getBroadcast(context, 1, playIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
-
         views.setOnClickPendingIntent(R.id.playButton, playPendingIntent);
 
         Intent stopIntent = new Intent(context, StopWatch.class);
@@ -86,6 +113,10 @@ public class StopWatch extends AppWidgetProvider {
         PendingIntent stopPendingIntent = PendingIntent.getBroadcast(context, 2, stopIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
         views.setOnClickPendingIntent(R.id.stopButton, stopPendingIntent);
 
+        Intent resetIntent = new Intent(context, StopWatch.class);
+        resetIntent.setAction("RESET_ACTION");
+        PendingIntent resetPendingIntent = PendingIntent.getBroadcast(context, 3, resetIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+        views.setOnClickPendingIntent(R.id.resetButton, resetPendingIntent);
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 }
