15f6547d598f314c0f4a36f09ee98592eb493623
livinoid98 <ccy9803@gmail.com>
2023-09-18 12:54:19 +0900

    :sparkle: Style(style/main): 메인 페이지 auth 핸들링 + 라우팅 처리 (#000)
    - 메인 페이지 각 소개 하단 버튼 페이지에 맞게 라우팅 처리 완료
    - 라우팅 되기 전 auth handling 메서드 적용 - 전역 isLogged 상태 확인 / 로그인이 되어있지 않으면 진입 불가

--- front/src/components/IconButton.tsx
+++ front/src/components/IconButton.tsx
@@ -1,12 +1,27 @@
 import { View, Text, Image, TouchableOpacity} from "react-native";
 import IconButtonLayout from "../styles/iconButtonLayout";
 import { useNavigation } from '@react-navigation/native';
+import IndexStore from "../stores/IndexStore";
 
-const IconButton = ({desc, title, iconImage} : any) => {
+const IconButton = ({desc, title, iconImage, movePage} : any) => {
   const navigation = useNavigation();
+  const {LoginStore} = IndexStore();
+
+  const authHandling = (pageName: string) => {
+    if(pageName === "Three"){
+      navigation.navigate(pageName);
+      return;
+    }
+
+    if(LoginStore.isLogged){
+      navigation.navigate(pageName);
+    }else{
+      alert("해당 서비스는 로그인 후 이용 가능합니다.");
+    }
+  }
   return(
         <>
-          <TouchableOpacity activeOpacity={0.7}>
+          <TouchableOpacity activeOpacity={0.7} onPress={() => authHandling(movePage)}>
             <View style={IconButtonLayout.iconButtonWrap}>
               <View>
                 <Text style={IconButtonLayout.iconButtonDesc}>{desc}</Text>
