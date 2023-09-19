f8569c937fe7852bec1673c00640dd1a6327ce89
lkk <rabbit1999k@naver.com>
2023-09-19 21:01:10 +0900

    :heavy_plus_sign: Feat(user): authorization code 기반 token 받아오기 구현 중

--- back/ppobbi/src/main/java/com/haru/ppobbi/global/util/oauth/OAuth2TokenHandler.java
+++ back/ppobbi/src/main/java/com/haru/ppobbi/global/util/oauth/OAuth2TokenHandler.java
@@ -4,15 +4,27 @@ import static com.haru.ppobbi.global.util.oauth.constant.OAuth2ExceptionMessage.
 import static com.haru.ppobbi.global.util.oauth.constant.OAuth2ExceptionMessage.INVALID_TOKEN;
 
 import com.haru.ppobbi.global.error.TokenException;
+import com.haru.ppobbi.global.util.oauth.OAuth2ResponseDto.OAuth2TokenInfo;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Component
 @RequiredArgsConstructor
@@ -20,6 +32,58 @@ import org.springframework.web.client.RestTemplate;
 public class OAuth2TokenHandler {
 
     private final RestTemplate restTemplate;
+
+
+    @Value("${spring.security.oauth2.client.registration.google.client-id}")
+    private String clientId;
+
+    @Value("${spring.security.oauth2.client.registration.google.client-secret}")
+    private String clientSecret;
+
+    @Value("${spring.security.oauth2.client.registration.google.redirect-uri}")
+    private String redirectURI;
+
+
+    public OAuth2TokenInfo getOAuthToken(String authorizationCode) {
+        String accessToken = "";
+        String refreshToken = "";
+
+        Map<String, Object> userAttribute;
+
+        // Header 생성
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        String googleAPI = "https://oauth2.googleapis.com/token";
+        URI googleURI = UriComponentsBuilder
+            .fromUriString(googleAPI)
+            .encode(StandardCharsets.UTF_8)
+            .build()
+            .toUri();
+
+        log.debug("[DEBUG/getOAuthToken] targetURL : {}", googleURI);
+
+        MultiValueMap<String, String> requestMap = new LinkedMultiValueMap();
+        requestMap.add("grant_type", "authorization_code");
+        requestMap.add("client_id", clientId);
+        requestMap.add("redirect_uri", redirectURI);
+        requestMap.add("code", authorizationCode);
+
+        ResponseEntity<HashMap> response;
+        try {
+            response = restTemplate.exchange(googleURI, HttpMethod.POST, entity,
+                HashMap.class);
+        } catch (HttpClientErrorException e) {
+            e.printStackTrace();
+            throw new TokenException(INVALID_TOKEN.message());
+        }
+        log.debug("[DEBUG/getOAuthToken] response : {}", response);
+
+        return OAuth2TokenInfo.builder()
+            .accessToken(accessToken)
+            .refreshToken(refreshToken)
+            .build();
+    }
 
     public String validateAccessTokenAndGetUserId(String accessToken) {
         // TODO : access token invalid -> refresh token 유효성 검증 -> 새로 access token 발급 구현
@@ -38,6 +102,13 @@ public class OAuth2TokenHandler {
             String errorMessage = e.getMessage();
             log.debug("error: {}", errorMessage);
             if (errorMessage.contains("invalid_token")) {
+                // TODO: refresh token으로 access token 받아오기
+                if (validateRefreshToken()) {
+                    accessToken = getAccessTokenFromRefreshToken();
+
+                } else {
+                    throw new TokenException(INVALID_TOKEN.message());
+                }
                 throw new TokenException(INVALID_TOKEN.message());
             } else if (errorMessage.contains("expired_token")) {
                 throw new TokenException(EXPIRED_TOKEN.message());
@@ -47,5 +118,13 @@ public class OAuth2TokenHandler {
         } catch (ParseException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private String getAccessTokenFromRefreshToken() {
+        return null;
+    }
+
+    private boolean validateRefreshToken() {
+        return true;
     }
 }
