30ecdda4f7561ea5db8e7b175a28f28df1fba1e7
Geon <quso12358@naver.com>
2023-09-19 10:57:00 +0900

    Feat(grave/error): Grave Error Handling 및 코드 리팩토링
    - JWT Filter 정규표현식 수정
    - Grave Service : grave 생성 및 조회 실패 케이스error handling
    - GraveRequestDto 및 GraveResponseDto 추가 및 변경 적용

--- back/ppobbi/src/main/java/com/haru/ppobbi/domain/grave/service/GraveService.java
+++ back/ppobbi/src/main/java/com/haru/ppobbi/domain/grave/service/GraveService.java
@@ -1,6 +1,7 @@
 package com.haru.ppobbi.domain.grave.service;
 
-import com.haru.ppobbi.domain.grave.dto.GraveRequestDto;
+import com.haru.ppobbi.domain.grave.dto.GraveRequestDto.RegistRequestDto;
+import com.haru.ppobbi.domain.grave.dto.GraveResponseDto.GraveInfoDto;
 import com.haru.ppobbi.domain.grave.entity.Grave;
 
 import java.util.List;
@@ -10,23 +11,23 @@ public interface GraveService{
      * GraveRequestDto 객체를 받아 등록
      * @author Geon
      * @param userId 관련된 User 기본 키
-     * @param graveRequestDto 등록할 graveRequestDto 객체
+     * @param registRequestDto 등록할 registRequestDto 객체
      * @return Grave
      */
-    public Grave registGrave(String userId, GraveRequestDto graveRequestDto);
+    public Grave registGrave(String userId, RegistRequestDto registRequestDto);
     /**
      * graveNo를 받아 Grave 조회
      * @author Geon
      * @param graveNo 조회할 Grave 기본 키
      * @return Grave
      */
-    public Grave selectGrave(Integer graveNo);
+    public GraveInfoDto selectGrave(Integer graveNo);
     /**
      * 모든 Grave 조회
      * @author Geon
      * @return {@code List<Grave>}
      */
-    public List<Grave> selectGraves();
+    public List<GraveInfoDto> selectGraves();
     /**
      * userNo를 받아 모든 Grave 조회
      * @author Geon
