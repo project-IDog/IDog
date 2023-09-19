5de5d3136271b3d102bb3d80e90ed2f85b3c535d
kimmainsain <kmsid003@gmail.com>
2023-09-19 13:43:17 +0900

    :heavy_plus_sign: Feat(Login) : axios.ts Secure JWT 추가, Login.tsx에 Store login TF관리

--- front/src/screens/Login.tsx
+++ front/src/screens/Login.tsx
@@ -4,32 +4,53 @@ import * as Google from "expo-auth-session/providers/google";
 import * as SecureStore from "expo-secure-store";
 import { Button, View } from "react-native";
 import { ANDROID_CLIENT_ID } from "@env";
+// import axios from "../utils/axios";
+import axios from "axios";
+import IndexStore from "../stores/IndexStore";
 
 WebBrowser.maybeCompleteAuthSession();
 
 const Login = () => {
+	const { LoginStore } = IndexStore();
+	console.log(LoginStore);
 	const [request, response, promptAsync] = Google.useAuthRequest({
 		androidClientId: ANDROID_CLIENT_ID,
 	});
-
 	const handleSignInWithGoogle = async () => {
 		if (response?.type === "success") {
-			const tokenData = {
-				idToken: response.authentication?.idToken,
-				refreshToken: response.authentication?.refreshToken,
-				accessToken: response.authentication?.accessToken,
-			};
-			await SecureStore.setItemAsync("token", JSON.stringify(tokenData));
+			const code = response.params?.code;
+			try {
+				const { data } = await axios.post(
+					"/user",
+					{
+						idToken: response.authentication?.idToken,
+						refreshToken: response.authentication?.refreshToken,
+						accessToken: response.authentication?.accessToken,
+					},
+					{
+						headers: {
+							"Content-Type": "application/json",
+							Authorization: "Bearer " + code,
+						},
+					},
+				);
+				console.warn(data.data.data);
+				await SecureStore.setItemAsync("JWT", data.data.data);
+				LoginStore.isLogged = true;
+			} catch (error) {
+				console.error(error);
+			}
 		}
 	};
 
 	const handleLogout = async () => {
-		await SecureStore.deleteItemAsync("token");
+		LoginStore.isLogged = true;
+		await SecureStore.deleteItemAsync("JWT");
 	};
 
 	const getSecureStorage = async () => {
-		const token = await SecureStore.getItemAsync("token");
-		console.warn(JSON.parse(token || "{}"));
+		const JWT = await SecureStore.getItemAsync("JWT");
+		console.warn(JSON.parse(JWT || "{}"));
 	};
 
 	// Google 인증 응답이 바뀔때마다 실행
