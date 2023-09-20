bf3bff9d6906cad965f0f9a9c6490355d4aaea80
Geon <quso12358@naver.com>
2023-09-20 10:03:26 +0900

    Refactor: Grave, Comment, Photo Controller 반환 타입 명시

--- back/ppobbi/src/main/java/com/haru/ppobbi/domain/photo/controller/PhotoController.java
+++ back/ppobbi/src/main/java/com/haru/ppobbi/domain/photo/controller/PhotoController.java
@@ -4,6 +4,7 @@ import static com.haru.ppobbi.domain.photo.constant.PhotoResponseMessage.*;
 
 import com.haru.ppobbi.domain.photo.constant.PhotoResponseMessage;
 import com.haru.ppobbi.domain.photo.dto.PhotoRequestDto.RegistRequestDto;
+import com.haru.ppobbi.domain.photo.dto.PhotoResponseDto.PhotoInfoDto;
 import com.haru.ppobbi.domain.photo.entity.Photo;
 import com.haru.ppobbi.domain.photo.service.PhotoService;
 import com.haru.ppobbi.global.dto.ResponseDto;
@@ -23,39 +24,39 @@ public class PhotoController {
     private final PhotoService photoService;
 
     @PostMapping()
-    public ResponseEntity<ResponseDto<?>> registPhoto(@RequestAttribute("userId") String userId, @RequestBody RegistRequestDto registRequestDto){
+    public ResponseEntity<ResponseDto<String>> registPhoto(@RequestAttribute("userId") String userId, @RequestBody RegistRequestDto registRequestDto){
         Photo photo = photoService.registPhoto(userId, registRequestDto);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(ResponseDto.create(CREATE_SUCCESS));
     }
 
     @GetMapping("/user/{userNo}")
-    public ResponseEntity<ResponseDto<?>> getAllAlbum(@PathVariable Integer userNo){
+    public ResponseEntity<ResponseDto<List<PhotoInfoDto>>> getAllAlbum(@PathVariable Integer userNo){
         return ResponseEntity.status(HttpStatus.OK)
                 .body(ResponseDto.create(READ_SUCCESS, photoService.selectPhotosByUserNo(userNo)));
     }
 
     @GetMapping("/{photoNo}")
-    public ResponseEntity<ResponseDto<?>> getPhotoInfo(@PathVariable Integer photoNo){
+    public ResponseEntity<ResponseDto<PhotoInfoDto>> getPhotoInfo(@PathVariable Integer photoNo){
         return ResponseEntity.status(HttpStatus.OK)
                 .body(ResponseDto.create(READ_SUCCESS, photoService.selectPhoto(photoNo)));
     }
 
     @DeleteMapping("/{photoNo}")
-    public ResponseEntity<ResponseDto<?>> deletePhoto(@PathVariable Integer photoNo){
+    public ResponseEntity<ResponseDto<String>> deletePhoto(@PathVariable Integer photoNo){
         photoService.deletePhoto(photoNo);
         return ResponseEntity.status(HttpStatus.OK)
                 .body(ResponseDto.create(DELETE_SUCCESS));
     }
 
     @GetMapping("/dog/{dogNo}")
-    public ResponseEntity<ResponseDto<?>> getAlbum(@PathVariable Integer dogNo){
+    public ResponseEntity<ResponseDto<List<PhotoInfoDto>>> getAlbum(@PathVariable Integer dogNo){
         return ResponseEntity.status(HttpStatus.OK)
                 .body(ResponseDto.create(READ_SUCCESS, photoService.selectPhotosByDogNo(dogNo)));
     }
 
     @PutMapping("/{photoNo}")
-    public ResponseEntity<ResponseDto<?>> updatePhotoGoat(@PathVariable Integer photoNo){
+    public ResponseEntity<ResponseDto<String>> updatePhotoGoat(@PathVariable Integer photoNo){
         photoService.setGoatPhoto(photoNo);
         return ResponseEntity.status(HttpStatus.OK)
                 .body(ResponseDto.create(UPDATE_SUCCESS));
