23833eb0bc89807ff764a08806fda6806844bb3e
kimmainsain <kmsid003@gmail.com>
2023-09-20 12:54:19 +0900

    :heavy_plus_sign: Feat(Login) : 앱 종료해도 모든 정보 유지 완료, Login 테스트코드 수정안한상태

--- front/src/components/CurrentAppState.tsx
+++ front/src/components/CurrentAppState.tsx
@@ -1,27 +1,24 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useEffect } from "react";
 import { AppState } from "react-native";
 import IndexStore from "../stores/IndexStore";
 import * as SecureStore from "expo-secure-store";
 
 const CurrentAppState = () => {
 	const appState = useRef(AppState.currentState);
-	const [appStateVisible, setAppStateVisible] = useState(appState.current);
 	const stores = IndexStore();
 
 	useEffect(() => {
+		secureStorageToStore();
 		const subscription = AppState.addEventListener("change", (nextAppState) => {
 			if (appState.current.match(/background/) && nextAppState === "active") {
 				secureStorageToStore();
-				console.log("App has come to the foreground!");
 			} else if (
 				appState.current.match(/active/) &&
 				nextAppState === "background"
 			) {
 				storeToSecureStorage();
-				console.log("App has come to the background!");
 			}
 			appState.current = nextAppState;
-			setAppStateVisible(appState.current);
 		});
 
 		return () => {
