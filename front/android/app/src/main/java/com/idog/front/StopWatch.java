98f20a691f76f802c7c7b067f4c0c171f4084df3
kimmainsain <kmsid003@gmail.com>
2023-09-22 00:19:54 +0900

    :heavy_plus_sign: Feat(widget) : counter -> stopwatch 위젯에서만 작동중, 개선필요

--- front/android/app/src/main/java/com/idog/front/StopWatch.java
+++ front/android/app/src/main/java/com/idog/front/StopWatch.java
@@ -1,9 +1,11 @@
 package com.idog.front;
 
+import android.annotation.SuppressLint;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.Looper;
 import android.widget.RemoteViews;
 import android.app.PendingIntent;
 import android.content.Intent;
@@ -13,11 +15,15 @@ import android.util.Log;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.WritableMap;
+import android.os.Handler;
+import android.os.Message;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class StopWatch extends AppWidgetProvider {
+    private static boolean isRunning = false;
+    private static Handler handler;
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
@@ -39,30 +45,48 @@ public class StopWatch extends AppWidgetProvider {
         // ...
     }
 
+    @SuppressLint("HandlerLeak")
     @Override
     public void onReceive(Context context, Intent intent) {
         super.onReceive(context, intent);
-        Log.d("StopWatch", "Received intent: " + intent.getAction());
-        Log.d("Context : ", "Context : " + context);
-        int appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
-        Log.d("appWidgetId", "appWidgetId : " + appWidgetId);
+
         SharedPreferences prefs = context.getSharedPreferences("MyWidget", Context.MODE_PRIVATE);
+        int appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+
         if ("PLAY_ACTION".equals(intent.getAction())) {
-            Log.d("WATCH_PLAY_ACTION", "WATCH_PLAY ACTION CLICK");
-            int number = prefs.getInt("number_" + appWidgetId, 0);
-            prefs.edit().putInt("number_" + appWidgetId, number + 1).apply();
-            updateAppWidget(context, AppWidgetManager.getInstance(context), appWidgetId);
+            if (!isRunning) {
+                isRunning = true;
+                Log.d("isRunning : ", "PLAY_ACTION ");
+                if (handler == null) {
+                    handler = new Handler(Looper.getMainLooper()) {
+                        @Override
+                        public void handleMessage(Message msg) {
+                            int appWidgetId = msg.what;
+                            int number = prefs.getInt("number_" + appWidgetId, 0);
+                            prefs.edit().putInt("number_" + appWidgetId, number + 1).apply();
+                            updateAppWidget(context, AppWidgetManager.getInstance(context), appWidgetId);
+                            if (isRunning) {
+                                handler.sendMessageDelayed(handler.obtainMessage(appWidgetId), 1000);
+                            }
+                        }
+                    };
+                }
+                handler.sendMessage(handler.obtainMessage(appWidgetId));
+            }
         } else if ("STOP_ACTION".equals(intent.getAction())) {
-            Log.d("WATCH_STOP_ACTION", "WATCH_STOP ACTION CLICK");
-            int number = prefs.getInt("number_" + appWidgetId, 0);
-            prefs.edit().putInt("number_" + appWidgetId, number + 10).apply();
-            updateAppWidget(context, AppWidgetManager.getInstance(context), appWidgetId);
+            isRunning = false;
+            Log.d("isRunning : ", "PLAY_ACTION ");
+            if (handler != null) {
+                Log.d("isRunning : ", "removeMessages ");
+                handler.removeMessages(appWidgetId);
+            }
         } else if ("RESET_ACTION".equals(intent.getAction())) {
-            Log.d("WATCH_RESET_ACTION", "WATCH_RESET_ACTION CLICK");
+            isRunning = false;
+            if (handler != null) {
+                handler.removeMessages(appWidgetId);
+            }
             prefs.edit().putInt("number_" + appWidgetId, 0).apply();
             updateAppWidget(context, AppWidgetManager.getInstance(context), appWidgetId);
-        } else if (TimerService.ACTION_UPDATE.equals(intent.getAction())) {
-            Log.d("WATCH_WHAT_ACTION", "WATCH_WHAT ACTION CLICK");
         }
     }
 
@@ -76,19 +100,19 @@ public class StopWatch extends AppWidgetProvider {
         Intent playIntent = new Intent(context, StopWatch.class);
         playIntent.setAction("PLAY_ACTION");
         playIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
-        PendingIntent playPendingIntent = PendingIntent.getBroadcast(context, 1, playIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+        PendingIntent playPendingIntent = PendingIntent.getBroadcast(context, 0, playIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
         views.setOnClickPendingIntent(R.id.playButton, playPendingIntent);
 
         Intent stopIntent = new Intent(context, StopWatch.class);
         stopIntent.setAction("STOP_ACTION");
         stopIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
-        PendingIntent stopPendingIntent = PendingIntent.getBroadcast(context, 2, stopIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+        PendingIntent stopPendingIntent = PendingIntent.getBroadcast(context, 0, stopIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
         views.setOnClickPendingIntent(R.id.stopButton, stopPendingIntent);
 
         Intent resetIntent = new Intent(context, StopWatch.class);
         resetIntent.setAction("RESET_ACTION");
         resetIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
-        PendingIntent resetPendingIntent = PendingIntent.getBroadcast(context, 3, resetIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+        PendingIntent resetPendingIntent = PendingIntent.getBroadcast(context, 0, resetIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
         views.setOnClickPendingIntent(R.id.resetButton, resetPendingIntent);
 
         appWidgetManager.updateAppWidget(appWidgetId, views);
