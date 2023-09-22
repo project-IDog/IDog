6e5b6c9b8ee41c0f69046974bfea180ab555c6e7
kimmainsain <kmsid003@gmail.com>
2023-09-22 14:39:07 +0900

    :heavy_plus_sign: Feat(widget) : 앱 stopwatch 적용완료, 위젯 디자인 변경예정

--- front/src/screens/WidgetText.tsx
+++ front/src/screens/WidgetText.tsx
@@ -1,12 +1,14 @@
-import { set } from "mobx";
 import React, { useEffect, useState } from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, Image, TouchableOpacity } from "react-native";
 import { NativeModules, DeviceEventEmitter } from "react-native";
-
+import WalkLayout from "../styles/walkLayout";
+import TimerImg from "../../assets/images/timer.png";
+import { set } from "mobx";
 const { StopWatchModule } = NativeModules;
 
 const WidgetText = () => {
 	const [widgetData, setWidgetData] = useState("00:00:00");
+	const [isPlaying, setIsPlaying] = useState(false);
 
 	useEffect(() => {
 		getWidgetData();
@@ -31,25 +33,49 @@ const WidgetText = () => {
 
 	const playTimer = () => {
 		StopWatchModule.playTimer();
+		setIsPlaying(true);
 	};
 
 	const stopTimer = () => {
 		StopWatchModule.stopTimer();
+		setIsPlaying(false);
 	};
 
 	const resetTimer = () => {
 		StopWatchModule.resetTimer();
 		setWidgetData("00:00:00");
+		setIsPlaying(false);
 	};
 
 	return (
-		<View>
-			<Text>{widgetData}</Text>
-			<Button title="get widget data" onPress={() => getWidgetData()} />
-			<Text>Current state</Text>
-			<Button title="Play" onPress={playTimer} />
-			<Button title="Stop" onPress={stopTimer} />
-			<Button title="Reset" onPress={resetTimer} />
+		<View style={WalkLayout.contentWrap}>
+			<Text style={WalkLayout.todayTimerTitle}>오늘의 산책시간</Text>
+			<View style={WalkLayout.timer}>
+				<Image source={TimerImg} style={WalkLayout.timerImg} />
+				<Text style={WalkLayout.timerText}>{widgetData}</Text>
+			</View>
+
+			<View style={WalkLayout.todayTimerButtonWrap}>
+				{!isPlaying ? (
+					<TouchableOpacity activeOpacity={0.7} onPress={playTimer}>
+						<View style={WalkLayout.startButton}>
+							<Text style={WalkLayout.startButtonText}>산책 시작</Text>
+						</View>
+					</TouchableOpacity>
+				) : (
+					<TouchableOpacity activeOpacity={0.7} onPress={stopTimer}>
+						<View style={WalkLayout.stopButton}>
+							<Text style={WalkLayout.stopButtonText}>일시 정지</Text>
+						</View>
+					</TouchableOpacity>
+				)}
+
+				<TouchableOpacity activeOpacity={0.7} onPress={resetTimer}>
+					<View style={WalkLayout.finishButton}>
+						<Text style={WalkLayout.finishButtonText}>산책 종료</Text>
+					</View>
+				</TouchableOpacity>
+			</View>
 		</View>
 	);
 };
