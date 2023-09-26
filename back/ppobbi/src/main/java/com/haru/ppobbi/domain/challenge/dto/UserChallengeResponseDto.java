f67702619ab62bd4a60846cc3445242e6343a5c2
Geon <quso12358@naver.com>
2023-09-26 10:40:33 +0900

    Feat(userchallenge): UserChallenge Service 구현

--- back/ppobbi/src/main/java/com/haru/ppobbi/domain/challenge/dto/UserChallengeResponseDto.java
+++ back/ppobbi/src/main/java/com/haru/ppobbi/domain/challenge/dto/UserChallengeResponseDto.java
@@ -1,10 +1,13 @@
 package com.haru.ppobbi.domain.challenge.dto;
 
+import com.haru.ppobbi.domain.challenge.entity.UserChallenge;
+import com.haru.ppobbi.global.constant.BaseConstant;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class UserChallengeResponseDto {
     @Getter
@@ -24,6 +27,16 @@ public class UserChallengeResponseDto {
     @AllArgsConstructor
     @Builder
     public static class UserChallengeListDto{
+        private Integer userChallengeNo;
+        private String challengeName;
+        private String challengeImageUrl;
+        private boolean userChallengeIsComplete;
 
+        public UserChallengeListDto(UserChallenge userChallenge){
+            this.userChallengeNo = userChallenge.getUserChallengeNo();
+            this.challengeName = userChallenge.getChallenge().getChallengeName();
+            this.challengeImageUrl = userChallenge.getChallenge().getChallengeImageUrl();
+            this.userChallengeIsComplete = Objects.equals(userChallenge.getUserChallengeIscomplete(), BaseConstant.COMPLETED);
+        }
     }
 }
