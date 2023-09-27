9dcd6c49fcfb6901ee42ab5096cc0c87f727d886
Geon <quso12358@naver.com>
2023-09-27 14:03:43 +0900

    Feat(grave/flower): Flower DTO 추가

---
+++ back/ppobbi/src/main/java/com/haru/ppobbi/domain/grave/entity/Flower.java
@@ -0,0 +1,33 @@
+package com.haru.ppobbi.domain.grave.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@RedisHash(value = "flower")
+public class Flower {
+    @Id
+    private Integer graveNo;
+
+    private List<LocalDateTime> flowers;
+
+    @TimeToLive
+    private long ttl;
+
+    @Builder
+    Flower(Integer graveNo, long ttl){
+        this.graveNo = graveNo;
+        this.ttl = ttl;
+        this.flowers = new ArrayList<>();
+    }
+}
