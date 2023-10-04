a52c5aae587c03e90e23fb72a555885f89c26285
livinoid98 <ccy9803@gmail.com>
2023-10-04 22:06:46 +0900

    :heavy_plus_sign: Feat(function/more-pet): nft 수정하기 -> 상세보기 및 api 연동 수정 (#000)
    - nft 수정하기 -> 상세보기로 수정 해당 nft api 가져와서 conrtract 주소와 tokenId 같이 보여주기
    - adoption에 from을 secureStore에 있는 privateKey 가져오기로 수정

--- front/src/screens/CreateWalletMain.tsx
+++ front/src/screens/CreateWalletMain.tsx
@@ -6,8 +6,6 @@ import Footer from "../components/Footer";
 
 import CreateWalletMainLayout from "../styles/CreateWalletMainLayout";
 
-import WalletMainImg from "../../assets/images/wallet-main-img.png";
-
 const CreateWalletMain = ({navigation} : any) => {
     return(
         <>
@@ -17,10 +15,6 @@ const CreateWalletMain = ({navigation} : any) => {
                     <Text style={CreateWalletMainLayout.mainTitle}>지갑 설정</Text>
                     <Text style={CreateWalletMainLayout.subTitle}>기존 지갑을 불러오거나 새 지갑을 만드세요.</Text>
                 </View>
-                <Image
-                    source={WalletMainImg}
-                    style={CreateWalletMainLayout.mainWalletImg}
-                />
                 <View style={CreateWalletMainLayout.buttonWrap}>
                     <TouchableOpacity>
                         <View style={CreateWalletMainLayout.reButton}>
--- front/src/screens/EditProfile.tsx
+++ front/src/screens/EditProfile.tsx
@@ -1,17 +1,22 @@
-import {useState} from "react"
+import {useState,useEffect} from "react"
 import {View, Text, TextInput, Image, TouchableOpacity} from "react-native"
 import ColorHeader from "../components/ColorHeader"
 import CommonLayout from "../components/CommonLayout"
 import Footer from "../components/Footer"
 import DateTimePickerModal from "react-native-modal-datetime-picker"
+import * as SecureStore from 'expo-secure-store';
+
+import axios from "../utils/axios"
 
 import DatePickerIcon from "../../assets/images/date-picker-icon.png"
 import AddPlusIcon from "../../assets/images/add-plus-icon.png"
 
 import CreateProfileLayout from "../styles/createProfileLayout"
 
-const EditProfile = ({navigation} : any) => {
+const EditProfile = ({navigation, route} : any) => {
     const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+    const [selectedDog, setSelectedDog] = useState<Object>();
+    const [myWalletAddress, setMyWalletAddress] = useState<String>();
 
     const showDatePicker = () => {
         setDatePickerVisibility(true);
@@ -25,10 +30,27 @@ const EditProfile = ({navigation} : any) => {
         console.warn("사용자가 선택한 날짜: ", date);
         hideDatePicker();
     };
+
+    useEffect(() => {
+        const selectedNo = route.params.selectedNo;
+
+        axios.get(`/dog/${selectedNo}`).then((data) => {
+            if(data.status === 200){
+                setSelectedDog(data.data.data);
+            }
+        })
+
+        const getAddress = async () => {
+            const address = await SecureStore.getItemAsync("walletAddress");
+            setMyWalletAddress(String(address));
+        }
+
+        getAddress();
+    }, [])
     return(
         <>
             <CommonLayout>
-                <ColorHeader title="프로필 수정"/>
+                <ColorHeader title="반려견 상세보기"/>
                 <View style={CreateProfileLayout.createProfileTitleWrap}>
                     <Text style={CreateProfileLayout.createProfileDesc}>반려견 NFT</Text>
                     <Text style={CreateProfileLayout.createProfileTitle}>
@@ -36,30 +58,40 @@ const EditProfile = ({navigation} : any) => {
                         나의 반려견
                     </Text>
                 </View>
-                <TouchableOpacity activeOpacity={0.7}>
-                    <View style={CreateProfileLayout.imageUploadWrap}>
-                        <Image
-                            source={AddPlusIcon}
-                        />
-                        <Text>사진 등록하기</Text>
-                    </View>
-                </TouchableOpacity>
+                {
+                    selectedDog?.dogImg ?
+                    <Image
+                        source={{uri: selectedDog?.dogImg}}
+                        style={CreateProfileLayout.editShowDogImg}
+                    />
+                    :
+                    <></>
+                }
 
                 <View style={CreateProfileLayout.formWrap}>
 
                     <Text style={CreateProfileLayout.formTitle}>반려견의 이름을 입력해주세요.</Text>
-                    <TextInput style={CreateProfileLayout.formInput}></TextInput>
+                    <TextInput
+                        style={CreateProfileLayout.formInput}
+                        value={selectedDog?.dogName}    
+                    />
                     <Text style={CreateProfileLayout.formTitle}>반려견의 종을 입력해주세요.</Text>
-                    <TextInput style={CreateProfileLayout.formInput}></TextInput>
+                    <TextInput
+                        style={CreateProfileLayout.formInput}
+                        value={selectedDog?.dogBreed}
+                    />
                     <Text style={CreateProfileLayout.formTitle}>반려견의 성별을 입력해주세요.</Text>
-                    <TextInput style={CreateProfileLayout.formInput}></TextInput>
+                    <TextInput 
+                        style={CreateProfileLayout.formInput}
+                        value={selectedDog?.dogSex}
+                    />
                     <Text style={CreateProfileLayout.formTitle}>반려견의 생일을 입력해주세요.</Text>
                     <TouchableOpacity activeOpacity={0.7} onPress={showDatePicker}>
                         <View style={CreateProfileLayout.dateFormWrap}>
                             <Image
                                 source={DatePickerIcon}
                             />
-                            <Text style={CreateProfileLayout.dateFormText}>2023. 09. 04.</Text>
+                            <Text style={CreateProfileLayout.dateFormText}>{selectedDog?.dogBirthDate}</Text>
                         </View>
                     </TouchableOpacity>
                     <DateTimePickerModal
@@ -68,21 +100,23 @@ const EditProfile = ({navigation} : any) => {
                         onConfirm={handleConfirm}
                         onCancel={hideDatePicker}
                     />
-
+                    <Text style={CreateProfileLayout.formTitle}>내 지갑의 주소입니다.</Text>
+                    <TextInput
+                        style={CreateProfileLayout.formInput}
+                        value={myWalletAddress}
+                    />
+                    <Text style={CreateProfileLayout.formTitle}>내 반려견의 token id입니다.</Text>
+                    <TextInput
+                        style={CreateProfileLayout.formInput}
+                        value={String(selectedDog?.dogNft)}
+                    />
                 </View>
 
                 <View style={CreateProfileLayout.formButtonWrap}>
-                    <TouchableOpacity activeOpacity={0.7}>
+                    <TouchableOpacity activeOpacity={0.7} onPress={() => navigation.navigate('Profile')}>
                         <View style={CreateProfileLayout.submitButton}>
                             <Text style={CreateProfileLayout.submitButtonText}>
-                                앨범 등록하기
-                            </Text>
-                        </View>
-                    </TouchableOpacity>
-                    <TouchableOpacity activeOpacity={0.7} onPress={() => navigation.navigate('Profile')}>
-                        <View style={CreateProfileLayout.cancelButton}>
-                            <Text style={CreateProfileLayout.cancelButtonText}>
-                                취소하기
+                                목록보기
                             </Text>
                         </View>
                     </TouchableOpacity>
