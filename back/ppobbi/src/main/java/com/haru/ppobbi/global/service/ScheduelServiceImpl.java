8ec7e6147f0168670cd80937bada719a94186c34
lkk <rabbit1999k@naver.com>
2023-10-05 22:47:38 +0900

    :bug: Fix: NFT 토큰 아이디 Service 예외 처리

--- back/ppobbi/src/main/java/com/haru/ppobbi/global/service/ScheduelServiceImpl.java
+++ back/ppobbi/src/main/java/com/haru/ppobbi/global/service/ScheduelServiceImpl.java
@@ -1,5 +1,8 @@
 package com.haru.ppobbi.global.service;
 
+import static com.haru.ppobbi.domain.user.constant.UserExceptionMessage.USER_NOT_FOUND_EXCEPTION;
+import static com.haru.ppobbi.global.constant.BaseConstant.NOTCANCELED;
+
 import com.haru.ppobbi.domain.dog.entity.Dog;
 import com.haru.ppobbi.domain.dog.repo.DogRepository;
 import com.haru.ppobbi.domain.user.entity.User;
@@ -7,6 +10,8 @@ import com.haru.ppobbi.domain.user.repo.UserRepository;
 import com.haru.ppobbi.global.constant.BaseConstant;
 import com.haru.ppobbi.global.dto.PolygonJsonResponseDto;
 import com.haru.ppobbi.global.error.NotFoundException;
+import java.util.List;
+import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,16 +20,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
-import javax.annotation.PostConstruct;
-import java.util.List;
-
-import static com.haru.ppobbi.domain.user.constant.UserExceptionMessage.USER_NOT_FOUND_EXCEPTION;
-import static com.haru.ppobbi.global.constant.BaseConstant.NOTCANCELED;
-
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class ScheduelServiceImpl{
+public class ScheduelServiceImpl {
 
     private final DogRepository dogRepository;
     private final UserRepository userRepository;
@@ -40,32 +39,40 @@ public class ScheduelServiceImpl{
         webClient = WebClient.create(basePolygonScanApi);
     }
 
-//    @Scheduled(cron = "0 0/10 * * * *")
+    //    @Scheduled(cron = "0 0/10 * * * *")
     @Scheduled(cron = "0 * * * * *")
     public void updateDogNftByTokenId() { //10분마다
         //강아지 테이블에서 dog_hash에 값이 있는 것만 리스트업
-        List<Dog> dogList = dogRepository.findByDogHashIsNotNullAndCanceled(BaseConstant.NOTCANCELED);
+        List<Dog> dogList = dogRepository.findByDogHashIsNotNullAndCanceled(
+            BaseConstant.NOTCANCELED);
 
         //for문을 돌면서 address 값을 넣어서 api를 호출하고
         for (int i = 0; i < dogList.size(); i++) {
             Dog dog = dogList.get(i);
             Integer nowUserNo = dogList.get(i).getUserNo();
             User user = userRepository.findUserByUserNoAndCanceled(nowUserNo, NOTCANCELED)
-                    .orElseThrow(() -> new NotFoundException(USER_NOT_FOUND_EXCEPTION.message()));
+                .orElseThrow(() -> new NotFoundException(USER_NOT_FOUND_EXCEPTION.message()));
             String userAddress = user.getUserAddress();
-            String searchUri = "?module=account&action=tokennfttx&contractaddress=0xdB983532a92837Ee0faF0e67854993a858f621d2&address="
+            String searchUri =
+                "?module=account&action=tokennfttx&contractaddress=0xdB983532a92837Ee0faF0e67854993a858f621d2&address="
                     + userAddress
                     + "&startblock=0&endblock=99999999&page=1&offset=100&sort=asc&apikey="
                     + POLYGON_APP_KEY;
 
-            Mono<PolygonJsonResponseDto> response = webClient.get()
+            Mono<PolygonJsonResponseDto> response;
+            PolygonJsonResponseDto data;
+            try {
+                response = webClient.get()
                     .uri(searchUri)
                     .retrieve()
                     .bodyToMono(PolygonJsonResponseDto.class)
-                    .onErrorMap(err -> new Exception("민팅 후 토큰아이디 업데이트 실패",err));
+                    .onErrorMap(err -> new Exception("민팅 후 토큰아이디 업데이트 실패", err));
 
-            PolygonJsonResponseDto data = response.share().block();
-
+                data = response.share().block();
+            } catch (Exception e) { // 임시로 Exception 받게 함
+                log.debug("[scheduleService] 토큰 업데이트 실패");
+                continue;
+            }
             System.out.println(dog.getDogHash());
 
             //결과로 받은 값의 transaction hash를 비교하면서 dog_nft 업데이트를 진행
@@ -73,16 +80,13 @@ public class ScheduelServiceImpl{
                 String nowHash = data.getResult().get(j).getHash();
                 Integer tokenId = Integer.parseInt(data.getResult().get(j).getTokenID());
 
-                if(dog.getDogHash().equals(nowHash)){
+                if (dog.getDogHash().equals(nowHash)) {
                     dog.updateDogNft(tokenId);
                     dog.setDogHash(null);
                     dogRepository.save(dog);
                     break;
                 }
             }
-
         }
-
-
     }
 }
