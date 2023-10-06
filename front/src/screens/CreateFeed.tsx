82d2100c4985194d362670d4db0d202da0b55634
kimmainsain <kmsid003@gmail.com>
2023-10-06 10:34:53 +0900

    :sparkles: Style(album) : 사진 여러번 클릭방지

--- front/src/screens/CreateFeed.tsx
+++ front/src/screens/CreateFeed.tsx
@@ -1,9 +1,17 @@
-import React, {useState} from "react"
-import {View,Text,Image,TextInput,TouchableOpacity,Button,Platform} from "react-native"
+import React, { useState } from "react";
+import {
+	View,
+	Text,
+	Image,
+	TextInput,
+	TouchableOpacity,
+	Button,
+	Platform,
+} from "react-native";
 import CommonLayout from "../components/CommonLayout";
 import ColorHeader from "../components/ColorHeader";
 import Footer from "../components/Footer";
-import * as SecureStore from 'expo-secure-store';
+import * as SecureStore from "expo-secure-store";
 
 import * as ImagePicker from "expo-image-picker";
 import { S3 } from "aws-sdk";
@@ -13,19 +21,21 @@ import {
 	AWS_REGION,
 	AWS_BUCKET,
 } from "@env";
-import axios from "../utils/axios"
+import axios from "../utils/axios";
 
-import AddPlusIcon from "../../assets/images/add-plus-icon.png"
+import AddPlusIcon from "../../assets/images/add-plus-icon.png";
 
 import CreateFeedLayout from "../styles/createFeedLayout";
+import { set } from "mobx";
 
-const CreateFeed = ({navigation, route}: any) => {
+const CreateFeed = ({ navigation, route }: any) => {
 	const selectedId = route.params.selectedId;
-    // img uri 저장
+	// img uri 저장
 	const [imageUri, setImage] = useState<string | null>(null);
 	const [comment, setComment] = useState<string | null>(null);
+	const [clickButton, setClickButton] = useState<boolean>(false);
 
-    // s3 클라이언트 초기화
+	// s3 클라이언트 초기화
 	const s3 = new S3({
 		accessKeyId: AWS_ACCESS_KEY,
 		secretAccessKey: AWS_SECRET_ACCESS_KEY,
@@ -49,17 +59,19 @@ const CreateFeed = ({navigation, route}: any) => {
 			if (err) {
 				console.log("err", err);
 			} else {
-				axios.post('/photo', {
-					dogNo:selectedId,
-					photoUrl:data.Location,
-					photoComment:comment,
-					photoIsGoat:false,
-				}).then((data) => {
-					if(data.data.message === "사진 등록 완료"){
-						alert("앨범 등록이 완료되었습니다.");
-						navigation.replace('Album');
-					}
-				})
+				axios
+					.post("/photo", {
+						dogNo: selectedId,
+						photoUrl: data.Location,
+						photoComment: comment,
+						photoIsGoat: false,
+					})
+					.then((data) => {
+						if (data.data.message === "사진 등록 완료") {
+							alert("앨범 등록이 완료되었습니다.");
+							navigation.replace("Album");
+						}
+					});
 			}
 		});
 	};
@@ -97,58 +109,70 @@ const CreateFeed = ({navigation, route}: any) => {
 		}
 	};
 
-    const submitFeed = () => {
-        uploadImage(imageUri);
-    }
+	const submitFeed = () => {
+		if (clickButton) return;
+		if (imageUri === null) {
+			alert("이미지를 선택해주세요.");
+			return;
+		}
+		setClickButton(true);
+		uploadImage(imageUri);
+	};
 
-    return(
-        <>
-            <CommonLayout>
-                <ColorHeader title="앨범등록"/>
-                <View style={CreateFeedLayout.createTitle}>
-                    <Text style={CreateFeedLayout.createTitleDesc}>반려견 포토앨범</Text>
-                    <Text style={CreateFeedLayout.createMainTitle}>
-                        내 피드에 저장하는,{"\n"}
-                        나의 반려견
-                    </Text>
-                </View>
+	return (
+		<>
+			<CommonLayout>
+				<ColorHeader title="앨범등록" />
+				<View style={CreateFeedLayout.createTitle}>
+					<Text style={CreateFeedLayout.createTitleDesc}>반려견 포토앨범</Text>
+					<Text style={CreateFeedLayout.createMainTitle}>
+						내 피드에 저장하는,{"\n"}
+						나의 반려견
+					</Text>
+				</View>
 
-                <TouchableOpacity activeOpacity={0.7} onPress={pickImage}>
-                    <View style={CreateFeedLayout.photoUploadWrap}>
-                        <Image
-                            source={AddPlusIcon}
-                        />
-                        <Text>사진 등록하기</Text>
-                    </View>
-                </TouchableOpacity>
-                {imageUri && (
-                    <Image source={{ uri: imageUri }} style={CreateFeedLayout.imageOnLayout}/>
-                )}
-                <View>
-                    <TextInput
-                        style={CreateFeedLayout.createDescWrap}
+				<TouchableOpacity activeOpacity={0.7} onPress={pickImage}>
+					<View style={CreateFeedLayout.photoUploadWrap}>
+						<Image source={AddPlusIcon} />
+						<Text>사진 등록하기</Text>
+					</View>
+				</TouchableOpacity>
+				{imageUri && (
+					<Image
+						source={{ uri: imageUri }}
+						style={CreateFeedLayout.imageOnLayout}
+					/>
+				)}
+				<View>
+					<TextInput
+						style={CreateFeedLayout.createDescWrap}
 						value={comment}
-                        placeholder="문구 입력..."
+						placeholder="문구 입력..."
 						onChangeText={(text) => setComment(text)}
-                    />
-                </View>
+					/>
+				</View>
 
-                <View style={CreateFeedLayout.buttonWrap}>
-                    <TouchableOpacity activeOpacity={0.7} onPress={submitFeed}>
-                        <View style={CreateFeedLayout.submitButton}>
-                            <Text style={CreateFeedLayout.submitButtonText}>앨범 등록하기</Text>
-                        </View>
-                    </TouchableOpacity>
-                    <TouchableOpacity activeOpacity={0.7} onPress={() => navigation.navigate('Album')}>
-                        <View style={CreateFeedLayout.cancelButton}>
-                            <Text style={CreateFeedLayout.cancelButtonText}>취소하기</Text>
-                        </View>
-                    </TouchableOpacity>
-                </View>
-                <Footer/>
-            </CommonLayout>
-        </>
-    )
-}
+				<View style={CreateFeedLayout.buttonWrap}>
+					<TouchableOpacity activeOpacity={0.7} onPress={submitFeed}>
+						<View style={CreateFeedLayout.submitButton}>
+							<Text style={CreateFeedLayout.submitButtonText}>
+								앨범 등록하기
+							</Text>
+						</View>
+					</TouchableOpacity>
+					<TouchableOpacity
+						activeOpacity={0.7}
+						onPress={() => navigation.navigate("Album")}
+					>
+						<View style={CreateFeedLayout.cancelButton}>
+							<Text style={CreateFeedLayout.cancelButtonText}>취소하기</Text>
+						</View>
+					</TouchableOpacity>
+				</View>
+				<Footer />
+			</CommonLayout>
+		</>
+	);
+};
 
 export default CreateFeed;
